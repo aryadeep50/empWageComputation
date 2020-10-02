50d0f2a4a0372505e694d38b76804a7658a60e24
Aryadeep Mukherjee <aryadeep50@gmail.com>
2020-10-02 20:44:27 +0530

    first commit

---
+++ empWageComputation.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash -x
+
+echo "Welcome to Employee Wage Computation Program"
+
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ];
+then
+    echo "Employee is Present";
+else
+    echo "Employee is absent";
+fi
